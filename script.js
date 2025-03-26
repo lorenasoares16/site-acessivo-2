26862f7d4ebcd7182875bb27cd20a3dbbeedab28
lorena_soares1256 <164919178+lorenasoares16@users.noreply.github.com>
2025-03-26 14:56:04 -0300

    sei la kk

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let tamanhoAtualFonte= 1;
+    const aumantaFonteBotao= document,getElementById('aumanta-fonte');
+
+    aumentaFonteBotao addEventListener('click', funcional(){
+        tamanhoAtualFonte +=0.1;
+        document.body.style.fontSize= '${tamanhoAtualFonte'
+    })
+    const diminuirFonteBotao= document.getElementById('diminuir-fonte');
+    diminuirFonteBotao
+}
